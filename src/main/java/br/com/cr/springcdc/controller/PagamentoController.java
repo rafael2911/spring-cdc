c25624e43c7c324d3e7553d1fb8fb41e78b0b170
Rafael Carvalho <crcarvalho@outlook.com>
2019-04-22 10:25:55 -0300

    Usando RestTemplate

--- src/main/java/br/com/cr/springcdc/controller/PagamentoController.java
+++ src/main/java/br/com/cr/springcdc/controller/PagamentoController.java
@@ -4,10 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.cr.springcdc.model.CarrinhoCompras;
+import br.com.cr.springcdc.model.DadosPagamento;
 
 @Controller
 @RequestMapping("/pagamento")
@@ -16,14 +19,32 @@ public class PagamentoController {
 	@Autowired
 	private CarrinhoCompras carrinho;
 	
+	@Autowired
+	private RestTemplate restTemplate;
+	
 	@PostMapping("finalizar")
 	public ModelAndView finalizar(RedirectAttributes attr) {
 		
-		attr.addFlashAttribute("message", "Pagamento realizado com sucesso!");
+		String response = "";
 		
-		System.out.println(carrinho.getTotal());
+		try {
+			
+			String uri = "http://book-payment.herokuapp.com/payment";
+			response = restTemplate.postForObject(uri, new DadosPagamento(carrinho.getTotal()), String.class);
+			
+			attr.addFlashAttribute("message", response);
+			
+			System.out.println(response);
+			
+			return new ModelAndView("redirect:/produto");
+			
+		}catch (HttpClientErrorException ex) {
+			attr.addFlashAttribute("message", "Valor maior que o permitido.");
+			ex.printStackTrace();
+			return new ModelAndView("redirect:/produto");
+		}
+
 		
-		return new ModelAndView("redirect:/produto");
 	}
 	
 }
---
+++ src/main/java/br/com/cr/springcdc/model/DadosPagamento.java
@@ -0,0 +1,17 @@
+package br.com.cr.springcdc.model;
+
+import java.math.BigDecimal;
+
+public class DadosPagamento {
+
+	private BigDecimal value;
+
+	public DadosPagamento(BigDecimal value) {
+		this.value = value;
+	}
+	
+	public BigDecimal getValue() {
+		return value;
+	}
+	
+}
