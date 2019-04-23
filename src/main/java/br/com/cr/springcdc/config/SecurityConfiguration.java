45109005101873b1ace3a8f493126ca1230ae953
Rafael Carvalho <crcarvalho@outlook.com>
2019-04-22 23:30:20 -0300

    Autorizando acesso

--- src/main/java/br/com/cr/springcdc/config/SecurityConfiguration.java
+++ src/main/java/br/com/cr/springcdc/config/SecurityConfiguration.java
@@ -1,9 +1,27 @@
 package br.com.cr.springcdc.config;
 
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+			.antMatchers("/resources/**").permitAll()
+			.antMatchers("/carrinho/**").permitAll()
+			.antMatchers("/pagamento/**").permitAll()
+			.antMatchers("/produto/form").hasRole("ADMIN")
+			.antMatchers(HttpMethod.POST, "/produto").hasRole("ADMIN")
+			.antMatchers(HttpMethod.GET, "/produto").hasRole("ADMIN")
+			.antMatchers("/produto/**").permitAll()
+			.antMatchers("/").permitAll()
+			.anyRequest().authenticated()
+			.and().formLogin();
+	}
+	
 }
