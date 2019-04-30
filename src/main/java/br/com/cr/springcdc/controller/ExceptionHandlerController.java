402f62c3743dd6e19394bba4bbaa1b826a27b188
Rafael Carvalho <crcarvalho@outlook.com>
2019-04-30 17:16:22 -0300

    Tratamento de exceções

---
+++ src/main/java/br/com/cr/springcdc/controller/ExceptionHandlerController.java
@@ -0,0 +1,21 @@
+package br.com.cr.springcdc.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice
+public class ExceptionHandlerController {
+	
+	@ExceptionHandler(Exception.class)
+	public ModelAndView trataErrosNaAplicacao(Exception exception) {
+		
+		exception.printStackTrace();
+
+        ModelAndView modelAndView = new ModelAndView("error");
+        modelAndView.addObject("exception", exception);
+		
+		return modelAndView;
+	}
+	
+}
