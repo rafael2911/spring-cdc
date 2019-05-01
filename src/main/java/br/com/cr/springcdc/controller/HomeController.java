03889de4698278bed57afba5028ae32993e52018
Rafael Carvalho <crcarvalho@outlook.com>
2019-04-30 23:10:09 -0300

    Testando

--- src/main/java/br/com/cr/springcdc/controller/HomeController.java
+++ src/main/java/br/com/cr/springcdc/controller/HomeController.java
@@ -40,7 +40,7 @@ public class HomeController {
 		return modelAndView;
 	}
 	
-	@GetMapping("/url-magica-maluca-jfeowge7fsijg98mfksdgmslk33fsldkfsç8gh56urlf1f3f5f9")
+	@GetMapping("/url-magica-maluca-jfeowge7fsijg98mfksdgmslk33fsldkfsc8gh56urlf1f3f5f9")
 	@ResponseBody
 	//@Transactional
 	public String urlMagicaMaluca() {
